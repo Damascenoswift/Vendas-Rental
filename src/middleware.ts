9f55dca46dc2694ac7750c6821200fa478746973
Guilherme Damasceno <guilhermedamasceno@MacBook-Pro-de-Guilherme.local>
2026-01-27 13:36:00 -0400

    fix: allow public access to docx templates

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,9 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - templates/ (public docx templates)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|templates/|.*\\.(?:svg|png|jpg|jpeg|gif|webp|docx)$).*)',
     ],
 }
